961e807731933883d9f1ae296f86249fd0354e9d
Mher Barseghyan <mherr.barseghyann@gmail.com>
2023-12-20 19:01:03 +0400

    commit_

--- base/views.py
+++ base/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 # Create your views here.
 
 
+rooms = [
+    {'id':1, 'name':'Lets learn py!'},
+    {'id':1, 'name':'Design!'},
+    {'id':1, 'name':'Devs'},
+
+]
+
+
+
+
+
 def home(request):
-    return render(request, 'home.html')
+    context={'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
