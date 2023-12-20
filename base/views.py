48b073297978db657a396715791c7f7ffab58a9e
Mher Barseghyan <mherr.barseghyann@gmail.com>
2023-12-20 19:12:43 +0400

    room names clickable

--- base/views.py
+++ base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+
+
 # Create your views here.
 
 
 rooms = [
     {'id':1, 'name':'Lets learn py!'},
-    {'id':1, 'name':'Design!'},
-    {'id':1, 'name':'Devs'},
+    {'id':2, 'name':'Design!'},
+    {'id':3, 'name':'Devs'},
 
 ]
 
@@ -17,5 +19,10 @@ def home(request):
     context={'rooms':rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'base/room.html')
+def room(request, pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+    context={'room':room}
+    return render(request, 'base/room.html', context)
